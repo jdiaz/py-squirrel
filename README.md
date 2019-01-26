85f0487615d6c4673c546bf6a5fd7b9b355019d5
Jose Diaz <jrd.palacios@gmail.com>
2019-01-25 22:57:30 -0600

    Adding mysql connector

--- README.md
+++ README.md
@@ -5,8 +5,10 @@ Dynamically create an SQL script backup of a database.
 
 ### Usage
 
-1. Configure the DB object with appropriate db connection information
-2. Run ```./pysquirrel```
-3. Open / Run the `DATABASE__NAME.sql` produced. The file contains all relevant SQL statements to recreate and insert each record in the database.
+1. Run ```pip install mysql-connector-python```
+2. Configure the DB object with appropriate db connection information
+3. Run ```./pysquirrel```
+
+Open / Run the `DATABASE_NAME.sql` produced. The file contains all relevant SQL statements to recreate and insert each record in the database.
 
 ** Currently under development **
--- pysquirrel.py
+++ pysquirrel.py
@@ -1,24 +1,62 @@
 #!/usr/bin/env python3
 
+import mysql.connector
+from mysql.connector import errorcode
+
+
 DB = {
-    'port': 3600,
-    'host': 'localhost',
-    'dbname': '',
-    'username': '',
+  'user': 'u',
+  'password': 'p',
+  'host': '127.0.0.1',
+  'database': 'mydb',
+  'raise_on_warnings': True
 }
 
-def validdb():
+SHOW_TABLES = 'SHOW TABLES;'
+DESCRIBE_TABLE = 'DESCRIBE TABLE %s;'
+
+
+def getconnection(config):
+    try:
+        return mysql.connector.connect(**config)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+    else:
+        conn.close()
+        return None
+
+
+def is_valid_config():
     for key in DB:
-        if DB[key] == '':
+        if DB[key] == '' or DB[key] == None:
             return False
     return True
 
-def backup():
+
+def execute_query(cursor, query):
+    try:
+        cursor.execute(query)
+    except mysql.connector.Error as err:
+        print("Failed running query: {}".format(err))
+
+
+def backup(conn):
     print('Running backup')
+    cursor = conn.cursor()
+    execute_query(cursor, SHOW_TABLES)
+    tables = [table_name[0] for table_name in cursor]
+    print('Found the following tables: {}'.format(tables))
+
 
 if __name__ == '__main__':
-    print('pysquirrel...')
-    if not validdb():
-        print('Invalid db connection info. Please update DB object')
+    print('Squirelling away...')
+    if not is_valid_config():
+        print('Invalid db config info. Please update DB dictionary.')
         exit()
-    backup()   
+    conn = getconnection(DB)
+    backup(conn)   
